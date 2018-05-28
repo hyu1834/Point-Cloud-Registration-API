e8e3138befb76f28329292b434984cebffb7ea4d
Hiu Hong Yu <hiuyu@ucdavis.edu>
2018-05-28 15:02:15 -0700

    implemented some registration usage example

--- example/example_bisecting_linear_line_complex_registration.cpp
+++ example/example_bisecting_linear_line_complex_registration.cpp
@@ -9,9 +9,38 @@
 #include <Eigen/Dense>
 
 //Local Libraries
+#include <point_cloud_utils.h>
 #include <bisecting_linear_line_complex_registration.h>
 
 int main(int argc, char** argv)	{
+	if(argc < 4)	{
+		std::cerr << "Usage: distance_geometry_registration <SourceCorrespondence> <SourcePointCloud> <TargetCorrespondence>\n";
+		return -1;
+	}
 
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr sourceCorrespondence(new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr sourcePointCloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr targetCorrespondence(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+
+	// Import Point Cloud
+	importPointCloud<pcl::PointXYZRGB>(argv[1], sourceCorrespondence);
+	importPointCloud<pcl::PointXYZRGB>(argv[2], sourcePointCloud);
+	importPointCloud<pcl::PointXYZRGB>(argv[3], targetCorrespondence);
+
+	BisectingLinearLineComplexRegistration<pcl::PointXYZRGB>* bllcr = new BisectingLinearLineComplexRegistration<pcl::PointXYZRGB>();
+	if(!bllcr->computeTransformation(sourceCorrespondence, targetCorrespondence))	{
+		std::cerr << "Error: Unable to compute transformation\n";
+		return -1;
+	}
+
+	if(!bllcr->pointCloudTransformation(sourcePointCloud))	{
+		std::cerr << "Error: Unable to transform source point cloud\n";
+		return -1;
+	}
+
+	exportPointCloud<pcl::PointXYZRGB>("transformatedPointCloud.txt", sourcePointCloud);
+
+	delete bllcr;
 	return 0;
 }
--- example/example_distance_geometry_registration.cpp
+++ example/example_distance_geometry_registration.cpp
@@ -9,10 +9,32 @@
 #include <Eigen/Dense>
 
 //Local Libraries
+#include <point_cloud_utils.h>
 #include <distance_geometry_registration.h>
 
 
 int main(int argc, char** argv)	{
+	if(argc < 4)	{
+		std::cerr << "Usage: distance_geometry_registration <SourceCorrespondence> <SourcePointCloud> <TargetCorrespondence>\n";
+		return -1;
+	}
 
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr sourceCorrespondence(new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr sourcePointCloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr targetCorrespondence(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+
+	// Import Point Cloud
+	importPointCloud<pcl::PointXYZRGB>(argv[1], sourceCorrespondence);
+	importPointCloud<pcl::PointXYZRGB>(argv[2], sourcePointCloud);
+	importPointCloud<pcl::PointXYZRGB>(argv[3], targetCorrespondence);
+
+	DistanceGeometryRegistration<pcl::PointXYZRGB>* dgr = new DistanceGeometryRegistration<pcl::PointXYZRGB>();
+	dgr->pointCloudRegistration(sourceCorrespondence, targetCorrespondence, sourcePointCloud, THREAD_MODE::THREAD_DISABLE);
+	std::cout << "Transformation Error: " << dgr->estimateError(sourceCorrespondence, targetCorrespondence);
+
+	exportPointCloud<pcl::PointXYZRGB>("transformatedPointCloud.txt", sourcePointCloud);
+
+	delete dgr;
 	return 0;
 }
